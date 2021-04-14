a80985f7b02776be263a93456c83f31c659e88f1
Mark Salpeter <marksalpeter@gmail.com>
2021-04-14 15:06:40 +0200

    improvement(docs): added batch processing example

--- process_batch_example_test.go
+++ process_batch_example_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/deliveryhero/pipeline/example/processors"
 )
 
-func ExampleProcess() {
+func ExampleProcessBatch() {
 	// Create a context that times out after 5 seconds
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -17,10 +17,8 @@ func ExampleProcess() {
 	// Create a pipeline that emits 1-6 at a rate of one int per second
 	p := pipeline.Delay(ctx, time.Second, pipeline.Emit(1, 2, 3, 4, 5, 6))
 
-	// Use the Multipleir to multiply each int by 10
-	p = pipeline.Process(ctx, &processors.Multiplier{
-		Factor: 10,
-	}, p)
+	// Use the BatchMultipleir to multiply 2 adjacent numbers together
+	p = pipeline.ProcessBatch(ctx, 2, time.Minute, &processors.BatchMultiplier{}, p)
 
 	// Finally, lets print the results and see what happened
 	for result := range p {
@@ -28,10 +26,8 @@ func ExampleProcess() {
 	}
 
 	// Output
-	// result: 10
-	// result: 20
-	// result: 30
-	// result: 40
-	// result: 50
-	// error: could not multiply 6, context deadline exceeded
+	// result: 2
+	// result: 12
+	// error: could not multiply [5], context deadline exceeded
+	// error: could not multiply [6], context deadline exceeded
 }

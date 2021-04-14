abf71e01c300791edd7a53cf42dd9a8b2d605687
Mark Salpeter <marksalpeter@gmail.com>
2021-04-14 15:20:21 +0200

    feat: added a helper func for Processor creation

--- process.go
+++ process.go
@@ -2,18 +2,6 @@ package pipeline
 
 import "context"
 
-// Processor represents a blocking operation in a pipeline. Implementing `Processor` will allow you to add
-// business logic to your pipelines without directly managing channels. This simplifies your unit tests
-// and eliminates channel management related bugs.
-type Processor interface {
-	// Process processes an input and reurns an output or an error, if the output could not be processed.
-	// When the context is canceled, process should stop all blocking operations and return the `Context.Err()`.
-	Process(ctx context.Context, i interface{}) (interface{}, error)
-
-	// Cancel is called if process returns an error or if the context is canceled while there are still items in the `in <-chan interface{}`.
-	Cancel(i interface{}, err error)
-}
-
 // Process takes each input from the `in <-chan interface{}` and calls `Processor.Process` on it.
 // When `Processor.Process` returns an `interface{}`, it will be sent to the output `<-chan interface{}`.
 // If `Processor.Process` returns an error, `Processor.Cancel` will be called with the cooresponding input and error message.
---
+++ processor.go
@@ -0,0 +1,37 @@
+package pipeline
+
+import "context"
+
+// Processor represents a blocking operation in a pipeline. Implementing `Processor` will allow you to add
+// business logic to your pipelines without directly managing channels. This simplifies your unit tests
+// and eliminates channel management related bugs.
+type Processor interface {
+	// Process processes an input and reurns an output or an error, if the output could not be processed.
+	// When the context is canceled, process should stop all blocking operations and return the `Context.Err()`.
+	Process(ctx context.Context, i interface{}) (interface{}, error)
+
+	// Cancel is called if process returns an error or if the context is canceled while there are still items in the `in <-chan interface{}`.
+	Cancel(i interface{}, err error)
+}
+
+// NewProcesssor creates a process and cancel func
+func NewProcesssor(
+	process func(ctx context.Context, i interface{}) (interface{}, error),
+	cancel func(i interface{}, err error),
+) Processor {
+	return &processor{process, cancel}
+}
+
+// processor implements Processor
+type processor struct {
+	process func(ctx context.Context, i interface{}) (interface{}, error)
+	cancel  func(i interface{}, err error)
+}
+
+func (p *processor) Process(ctx context.Context, i interface{}) (interface{}, error) {
+	return p.process(ctx, i)
+}
+
+func (p *processor) Cancel(i interface{}, err error) {
+	p.cancel(i, err)
+}
